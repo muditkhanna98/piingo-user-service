667befc02b1f3dba491c454ed0a58b5a9c014484
Mudit Khanna <muditkhanna998@gmail.com>
2026-04-03 01:00:49 -0400

    Initial user service setup

---
+++ src/main/java/com/mudit/piingouserservice/PiingoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mudit.piingouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiingoUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiingoUserServiceApplication.class, args);
+    }
+
+}
